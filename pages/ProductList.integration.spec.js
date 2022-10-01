21549b86bc553d54d2ebb300a251a2f8d822959f
Clebson Moura <clebson.sh@gmail.com>
2022-10-01 14:03:01 -0300

    aula 21 done

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -19,83 +19,77 @@ describe('ProductList - integration', () => {
 
   afterEach(() => {
     server.shutdown();
+    jest.clearAllMocks();
   });
 
-  it('should mount the component', () => {
-    const wrapper = mount(ProductList);
-    expect(wrapper.vm).toBeDefined();
-  });
+  const mountProductList = async (
+    quantity = 10,
+    overrides = [],
+    shouldReject
+  ) => {
+    const products = await getProducts(quantity, overrides);
 
-  it('should mount the Search component', () => {
-    const wrapper = mount(ProductList);
-    expect(wrapper.findComponent(Search)).toBeDefined();
-  });
+    if (shouldReject) {
+      axios.get.mockReturnValue(
+        Promise.reject(new Error('Problemas ao carregar a lista!'))
+      );
+    } else {
+      axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+    }
 
-  it('should call axios.get on component mount', () => {
-    mount(ProductList, {
+    const wrapper = mount(ProductList, {
       mocks: {
         $axios: axios,
       },
     });
+
+    await nextTick();
+
+    return { products, wrapper };
+  };
+
+  const getProducts = (quantity = 10, overrides = []) => [
+    ...server.createList('product', quantity),
+    ...overrides.map((override) => server.create('product', override)),
+  ];
+
+  it('should mount the component', async () => {
+    const { wrapper } = await mountProductList();
+
+    expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should mount the Search component', async () => {
+    const { wrapper } = await mountProductList();
+
+    expect(wrapper.findComponent(Search)).toBeDefined();
+  });
+
+  it('should call axios.get on component mount', async () => {
+    await mountProductList();
 
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.get).toHaveBeenLastCalledWith('/api/products');
   });
 
   it('should mount the ProductCard component 10 times', async () => {
-    const products = server.createList('product', 10);
-
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
-
-    await nextTick();
+    const { wrapper } = await mountProductList();
 
     const cards = wrapper.findAllComponents(ProductCard);
     expect(cards).toHaveLength(10);
   });
 
   it('should display the error message wneh Promise rejects', async () => {
-    axios.get.mockReturnValue(
-      Promise.reject(new Error('Problemas ao carregar a lista!'))
-    );
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
-
-    await nextTick();
-
+    const { wrapper } = await mountProductList(10, [], true);
     expect(wrapper.text()).toContain('Problemas ao carregar a lista!');
   });
 
   it('should filter the product list when a search is performed', async () => {
     // Arrenge
-    const products = [
-      ...server.createList('product', 10),
-      server.create('product', {
-        title: 'Meu relógio amado',
-      }),
-      server.create('product', {
-        title: 'Meu outro relógio estimado',
-      }),
-    ];
-
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
-
-    await nextTick();
+    const { wrapper } = await mountProductList(10, [
+      { title: 'Meu relógio amado' },
+      { title: 'Meu outro relógio estimado' },
+    ]);
 
     // Act
     const search = wrapper.findComponent(Search);
@@ -110,22 +104,9 @@ describe('ProductList - integration', () => {
 
   it('should filter the product list when a search is performed', async () => {
     // Arrenge
-    const products = [
-      ...server.createList('product', 10),
-      server.create('product', {
-        title: 'Meu relógio amado',
-      }),
-    ];
-
-    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
-
-    const wrapper = mount(ProductList, {
-      mocks: {
-        $axios: axios,
-      },
-    });
-
-    await nextTick();
+    const { wrapper } = await mountProductList(10, [
+      { title: 'Meu relógio amado' },
+    ]);
 
     // Act
     const search = wrapper.findComponent(Search);
