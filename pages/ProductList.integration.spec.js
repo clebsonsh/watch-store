42f3f5deaaeaa24a08f9653540f248fd14d2bf7a
Clebson Moura <clebson.sh@gmail.com>
2022-10-01 13:14:24 -0300

    aulas 19 e 20 done

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -74,4 +74,69 @@ describe('ProductList - integration', () => {
 
     expect(wrapper.text()).toContain('Problemas ao carregar a lista!');
   });
+
+  it('should filter the product list when a search is performed', async () => {
+    // Arrenge
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+      server.create('product', {
+        title: 'Meu outro relógio estimado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await nextTick();
+
+    // Act
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+
+    // Assert
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.searchTerm).toEqual('relógio');
+    expect(cards).toHaveLength(2);
+  });
+
+  it('should filter the product list when a search is performed', async () => {
+    // Arrenge
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await nextTick();
+
+    // Act
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+    search.find('input[type="search"]').setValue('');
+    await search.find('form').trigger('submit');
+
+    // Assert
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.searchTerm).toEqual('');
+    expect(cards).toHaveLength(11);
+  });
 });
